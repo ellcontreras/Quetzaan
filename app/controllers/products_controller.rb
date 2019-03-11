fab298f32e7058cf9de0cf6ebf08a6c14c960926
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-03-11 12:07:26 -0600

    Created CRUD for products

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,46 @@
+class ProductsController < ApplicationController
+  def index
+    redirect_to root_path
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    @product.save
+    redirect_to @product
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+
+    @product.destroy
+    redirect_to products_path
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :description, :price, :quantity)
+    end
+end
