49cdc9269f3f4a06d4995e4b7ca165b95f1ead46
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-04-05 21:20:02 -0600

    Support for images

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :image, presence: true
+
+  has_attached_file :image, styles: {:medium => "650px"}
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
