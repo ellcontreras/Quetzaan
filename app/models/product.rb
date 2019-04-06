ec38a375ebed4c4542d8bf7850c2d624215576a1
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-04-05 23:24:24 -0600

    Updated model to carrierview

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,4 @@
 class Product < ApplicationRecord
-  validates :image, presence: true
-
-  has_attached_file :image, styles: {:medium => "650px"}
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  mount_uploaders :product_pictures, ProductPicturesUploader
+  serialize :product_pictures, JSON
 end
