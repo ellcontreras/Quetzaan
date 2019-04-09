3566439bff6adb4f0d159258907fb23b9387efe5
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-04-09 13:49:45 -0500

    Working

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
+  include AlgoliaSearch
+
   mount_uploaders :product_pictures, ProductPicturesUploader
   serialize :product_pictures, JSON
+
+  algoliasearch do
+    attribute :name
+  end
 end
