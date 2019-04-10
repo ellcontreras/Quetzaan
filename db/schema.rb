7416e3e2433d5f9acc97667c3df5ef3a3d8814c0
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-04-09 23:16:49 -0500

    Added new columns and relation between user and product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_07_001221) do
+ActiveRecord::Schema.define(version: 2019_04_10_031608) do
 
   create_table "products", force: :cascade do |t|
     t.text "name"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2019_04_07_001221) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "product_pictures"
+    t.integer "user_id"
+    t.string "category"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
