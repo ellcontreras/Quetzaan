c5eb2bcda848949364af34b21485b07abbcf6a06
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-05-27 18:36:36 -0500

    Comunication between frontend and backend with ckecout

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -69,6 +69,6 @@ class CheckoutsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def checkout_params
-      params.require(:checkout).permit(:products_id, :total_amount, :user_id)
+      params.require(:checkout).permit(:products_id, :total_amount, :user_id, :status)
     end
 end
