3e3294b415aff83adbc882f94ebf11041244eb1f
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-05-31 22:32:57 -0500

    Remove error

--- app/assets/javascripts/cart-shop.js
+++ app/assets/javascripts/cart-shop.js
@@ -19,7 +19,7 @@ function commitProducts() {
   try {
     document.getElementById("cart_container").innerHTML = "";
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
   renderCartProducts();
 }
@@ -55,28 +55,33 @@ function deleteProductFromCart(id) {
 function renderCartProducts() {
   try {
     let cartContainer = document.getElementById("cart_container");
-  } catch (e) {
-    console.log(e)
-  }
-  let total = 0;
 
-  if (products.length > 0) {
-    products.forEach(product => {
-      cartContainer.innerHTML += `<div class="navbar-item"><button onClick="deleteProductFromCart(${
-        product.id
-      })" class="button is-danger is-small">
+    if (cartContainer != null) {
+      let total = 0;
+
+      if (products.length > 0) {
+        products.forEach(product => {
+          cartContainer.innerHTML += `<div class="navbar-item"><button onClick="deleteProductFromCart(${
+            product.id
+          })" class="button is-danger is-small">
     <span class="icon"><i class="fas fa-times"></i></span></button>
                       <span class="has-text-black">${product.name}</span>
                       <hr class="navbar-divider"></div>`;
 
-      total += parseInt(product.price);
-    });
+          total += parseInt(product.price);
+        });
 
-    document.getElementById("total_text").style.display = "block";
-    document.getElementById("total_text").innerHTML = `<b>Total: </b>$${total}`;
-  } else {
-    document.getElementById("total_text").style.display = "none";
-    cartContainer.innerHTML = `<div class="navbar-item"><p class="has-text-black">No has agregado productos al carrito</p></div>`;
+        document.getElementById("total_text").style.display = "block";
+        document.getElementById(
+          "total_text"
+        ).innerHTML = `<b>Total: </b>$${total}`;
+      } else {
+        document.getElementById("total_text").style.display = "none";
+        cartContainer.innerHTML = `<div class="navbar-item"><p class="has-text-black">No has agregado productos al carrito</p></div>`;
+      }
+    }
+  } catch (e) {
+    console.log(e);
   }
 }
 
