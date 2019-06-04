9e5fd2e95b0fc9244a95fcbe8b68a15b5cf93a56
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-06-03 21:11:26 -0500

    Remove turbolinks events

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,87 +18,85 @@
 //= require quill.min
 //= require quill.global
 
-document.addEventListener("turbolinks:load", function() {
-  console.log("asdcacd");
+console.log("asdcacd");
 
-  var defaults = {
-    theme: "snow",
-    modules: {
-      toolbar: [
-        [{ header: [1, 2, 3, false] }],
-        ["bold", "italic", "underline", "strike"],
-        ["blockquote", "code-block", "image", "link"],
-        [{ list: "ordered" }, { list: "bullet" }],
-        [{ indent: "-1" }, { indent: "+1" }],
-        ["clean"]
-      ]
-    }
-  };
+var defaults = {
+  theme: "snow",
+  modules: {
+    toolbar: [
+      [{ header: [1, 2, 3, false] }],
+      ["bold", "italic", "underline", "strike"],
+      ["blockquote", "code-block", "image", "link"],
+      [{ list: "ordered" }, { list: "bullet" }],
+      [{ indent: "-1" }, { indent: "+1" }],
+      ["clean"]
+    ]
+  }
+};
 
-  //This is the global config object
-  Quilljs.setDefaults(defaults);
+//This is the global config object
+Quilljs.setDefaults(defaults);
 
-  document.addEventListener("DOMContentLoaded", () => {
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(
-      document.querySelectorAll(".navbar-burger"),
-      0
-    );
+document.addEventListener("DOMContentLoaded", () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
 
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-      // Add a click event on each of them
-      $navbarBurgers.forEach(el => {
-        el.addEventListener("click", () => {
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+      el.addEventListener("click", () => {
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
 
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle("is-active");
-          $target.classList.toggle("is-active");
-        });
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
       });
-    }
-  });
-
-  var searchClient = algoliasearch(
-    "DNAY66KGWV",
-    "b78b16de41e0e3f75bbdd6a471fae704"
-  );
-
-  var search = instantsearch({
-    indexName: "Product",
-    searchClient,
-    searchFunction(helper) {
-      if (helper.state.query === "") {
-        return;
-      }
-
-      helper.search();
-    }
-  });
-
-  search.addWidget(
-    instantsearch.widgets.searchBox({
-      container: "#search_input",
-      showLoadingIndicator: true,
-      placeholder: "Ingresa tu busqueda..."
-    })
-  );
-
-  search.addWidget(
-    instantsearch.widgets.hits({
-      container: "#hits",
-      templates: {
-        empty:
-          "<br /><p class='has-text-centered'><b>No hay resultados que mostrar</b></p>",
-        item:
-          "<li><a href=/products/{{{ objectID }}}>{{{  _highlightResult.name.value}}}</a></li>"
-      },
-      transformItems: items => items.map(item => item)
-    })
-  );
-
-  search.start();
+    });
+  }
 });
+
+var searchClient = algoliasearch(
+  "DNAY66KGWV",
+  "b78b16de41e0e3f75bbdd6a471fae704"
+);
+
+var search = instantsearch({
+  indexName: "Product",
+  searchClient,
+  searchFunction(helper) {
+    if (helper.state.query === "") {
+      return;
+    }
+
+    helper.search();
+  }
+});
+
+search.addWidget(
+  instantsearch.widgets.searchBox({
+    container: "#search_input",
+    showLoadingIndicator: true,
+    placeholder: "Ingresa tu busqueda..."
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.hits({
+    container: "#hits",
+    templates: {
+      empty:
+        "<br /><p class='has-text-centered'><b>No hay resultados que mostrar</b></p>",
+      item:
+        "<li><a href=/products/{{{ objectID }}}>{{{  _highlightResult.name.value}}}</a></li>"
+    },
+    transformItems: items => items.map(item => item)
+  })
+);
+
+search.start();
