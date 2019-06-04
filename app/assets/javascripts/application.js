6356aa16c6afc21cc76aea9c4f575cf0b0335d29
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-06-03 19:30:27 -0500

    Testing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
+//= require rails
 //= require activestorage
 //= require turbolinks
 //= require_tree .
 //= require algolia/v3/algoliasearch.min
+//= require jquery
 //= require quill.min
 //= require quill.global
 
